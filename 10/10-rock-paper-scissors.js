0f55db9059e72e7fd9fe98a9e836302245a54f9f
First edit <buitho@oregonstate.edu>
2025-08-10 14:17:16 -0700

    organize code, put js and css separate

---
+++ 10/10-rock-paper-scissors.js
@@ -0,0 +1,90 @@
+const score = JSON.parse(localStorage.getItem('score')) || {
+  Wins: 0,
+  Losses: 0,
+  Ties: 0
+}
+
+// create computer's choice
+function createComputerChoice() {
+  const randomNum = Math.random(); 
+  let computerChoice = '';
+  if (randomNum < 1/3) {
+    computerChoice = 'rock' 
+  } else if (randomNum > 1/3 && randomNum < 2/3) {
+    computerChoice = 'paper'
+  } else {
+    computerChoice ='scissors'
+  };
+  return computerChoice;
+};
+
+// get user's choice and compare 
+function playGame(userChoice) {
+  // call function to get computer's choice first 
+  const computerChoice = createComputerChoice(); 
+  let result = '';
+
+  // check if tie
+  if (userChoice === computerChoice) {
+    result = 'Tie.'
+    score.Ties += 1;
+  } else if (userChoice === 'rock') {
+      if (computerChoice === 'paper') {
+      result = 'You lose.'
+      score.Losses += 1; 
+      } else if (computerChoice === 'scissors') {
+      result = 'You win.'
+      score.Wins += 1; 
+      };
+  } else if (userChoice === 'paper') {
+      if (computerChoice === 'rock') {
+      result = 'You win.'
+      score.Wins += 1; 
+      } else if (computerChoice === 'scissors') {
+      result = 'You lose.'
+      score.Losses += 1; 
+      };
+  } else if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+    result = 'You lose.'
+    score.Losses += 1; 
+  } else if (computerChoice === 'paper') {
+    result = 'You win.'
+    score.Wins += 1; 
+    };
+  };
+
+  // display result 
+  document.querySelector('.js-result').innerText = result; 
+
+  // display both player's Choices
+  document.querySelector('.js-choices').innerHTML = (`You <img class="move-img edit-img" src="game-images/${userChoice}.png"> <img class="move-img edit-img" src="game-images/${computerChoice}.png"> Computer`)
+
+  // update score 
+  document.querySelector('.js-score-paragraph').innerText = `Wins: ${score.Wins} Losses: ${score.Losses} Ties: ${score.Ties}`
+
+  // save score if user reloads
+  localStorage.setItem('score', JSON.stringify(score));
+};
+
+// display score to user on load 
+document.querySelector('.js-score-paragraph').innerText = `Wins: ${score.Wins} Losses: ${score.Losses} Ties: ${score.Ties}`
+
+// create function to reset score and update page
+function resetScore() {
+  score.Wins = 0
+  score.Losses = 0
+  score.Ties = 0
+
+  // remove result 
+  document.querySelector('.js-result').innerText = ''; 
+
+  // display both player's Choices
+  document.querySelector('.js-choices').innerText = '';
+
+  // display reset score
+  document.querySelector('.js-score-paragraph').innerHTML = `Wins: ${score.Wins} Losses: ${score.Losses} Ties: ${score.Ties}`
+
+  // save score if user reloads
+  localStorage.setItem('score', JSON.stringify(score));
+}
