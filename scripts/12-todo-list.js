2cc87234bc1011857baad199024b68f30736f6cd
First edit <buitho@oregonstate.edu>
2025-08-15 19:54:13 -0700

    add all files in lesson 12 part 1

---
+++ scripts/12-todo-list.js
@@ -0,0 +1,52 @@
+let todoList = []
+
+function updateArray() {
+  const userInput = document.querySelector('.js-input').value 
+  todoList.push(userInput);
+  console.log(todoList);
+
+  document.querySelector('.js-input').value = '';
+}
+
+function updateArray2() {
+  const userInput = document.querySelector('.js-input2').value 
+  const userInput3 = document.querySelector('.js-input3').value 
+  let tempObject = {
+    name: `${userInput}`,
+    date: `${userInput3}`
+  };
+
+  todoList.push(tempObject);
+  console.log(tempObject);
+
+  document.querySelector('.js-input2').value = '';
+  document.querySelector('.js-input3').value = '';
+  renderList();
+};
+
+// goal: take array and display it 
+function renderList() {
+  let i = 0;
+  let text = '';
+
+  todoList.forEach(function(todoObject, index) {
+    html = `
+    <div>${todoObject.name}</div>
+    <div>${todoObject.date}</div>
+    <button onclick="
+      todoList.splice(${index},1);
+      renderList();
+    " class="delete-todo-button">Delete</button>
+    `;
+    text += html 
+    document.querySelector('.js-todoList').innerHTML = text;
+  });
+};
+
+function triggerButton(event) {
+  if (event.key === 'Enter') {
+    updateArray2();
+    renderList();
+  };
+};
+
